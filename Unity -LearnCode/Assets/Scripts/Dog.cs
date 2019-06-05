60c03cb1bdd15c94594887fe820bd28121bb484a
petitlapin1204 <petitlapin1204@gmail.com>
2019-06-05 11:42:41 +0800

    新增Dog類別欄位及方法

---
+++ Unity -LearnCode/Assets/Scripts/Dog.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Dog
+{
+    #region 欄位 //region是程式碼
+    public string name;
+    public float weight;
+    public string type;
+    public string sex;
+    public string color;
+    public int age;
+    #endregion //有開頭就有結束
+
+    #region 方法
+    public void Bark()
+    {
+        Debug.Log(name + :"汪汪!!!");
+    }
+    #endregion
+
+}
