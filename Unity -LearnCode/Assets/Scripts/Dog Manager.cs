b8db0a4f7e93b2307b4e34c3b9b15b0cf0d46c6c
petitlapin1204 <petitlapin1204@gmail.com>
2019-06-05 13:59:01 +0800

    新增Dog類別.年齡轉換.吃東西的方法

---
+++ Unity -LearnCode/Assets/Scripts/Dog Manager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Dogmanger : MonoBehaviour
+{
+    # regio 欄位
+    public Dog DogA = new Dog();
+    public Dog DogB = new Dog();
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        DogA.name = "鬼娃";
+        DogA.weight = 30.5f;
+        DogA.type = "貴賓";
+        DogA.sex = "公";
+        DogA.color = "咖啡";
+        DogA.age = 7;
+
+        DogB.name = "恰吉";
+        DogB.weight = 25.5f;
+        DogB.type = "貴賓";
+        DogB.sex = "母";
+        DogB.color = "白色";
+        DogB.age = 5;
+    }
+    #endregion
+
+
+}
--- Unity -LearnCode/Assets/Scripts/Dog.cs
+++ Unity -LearnCode/Assets/Scripts/Dog.cs
@@ -12,10 +12,41 @@ public class Dog
     #endregion //有開頭就有結束
 
     #region 方法
-    public void Bark()
+    /// <summary>
+    /// 狗狗叫聲的方法
+    /// </summary>
+    public void Bark()//叫聲
     {
-        Debug.Log(name + :"汪汪!!!");
+        Debug.Log(name + ":汪汪!!!");
+    }
+    
+    /// <summary>
+    /// 轉換狗狗年齡的方法
+    /// </summary>
+    /// <returns>狗狗年齡*7</returns>
+    public int ConvertAge() 
+    {
+        return age * 7;
+    }
+    
+    /// <summary>
+    /// 狗狗吃東西的方法.速度
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="speed"></param>
+    public void Eat(string food,string speed = "慢")
+    {
+        Debug.Log(name + ":" + food + "，速度:" + speed); 
+    }
+
+    /// <summary>
+    /// 狗狗吃東西的方法.份量
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="count"></param>
+    public void Eat(string food,int count)
+    {
+        Debug.Log(name + ":" + food + "，份量:" + count);
     }
     #endregion
-
 }
